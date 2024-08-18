865f4d085596c6fb6b8384506ceded09bce13bd7
Usmana Zulfiqar <118796976+usmana5809@users.noreply.github.com>
2024-08-18 22:41:29 +0500

    update

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,12 +1,7 @@
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
-
-// Also install this npm i --save-dev @types/react-lottie
 import Lottie from "react-lottie";
-
 import { cn } from "@/lib/utils";
-
-
 import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
@@ -37,7 +32,6 @@ export const BentoGridItem = ({
   id,
   title,
   description,
-  //   remove unecessary things here
   img,
   imgClassName,
   titleClassName,
@@ -52,8 +46,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "Express", "Node.js", "Python", "Bootstrap"];
+  const rightLists = ["MOngodb", "Reactnative", "Html/Css","Django",];
 
   const [copied, setCopied] = useState(false);
 
@@ -82,9 +76,8 @@ export const BentoGridItem = ({
       style={{
         //   add these two
         //   you can generate the color from here https://cssgradient.io/
-        background: "rgb(4,7,29)",
-        backgroundColor:
-          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+        background: 'rgb(2,0,36)',
+        backgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(34,139,34,1) 51%, rgba(175,225,175,1) 100%)',
       }}
     >
       {/* add img divs */}
